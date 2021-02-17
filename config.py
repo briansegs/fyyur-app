68c225bc8bfd34fb0c9ed3a2a123378ee1cfaed2
brian <briansegs@gmail.com>
2021-02-17 13:00:49 -0500

    Remove debug and db

--- config.py
+++ config.py
@@ -3,9 +3,3 @@ SECRET_KEY = os.urandom(32)
 # Grabs the folder where the script runs.
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-# Enable debug mode.
-DEBUG = True
-
-# Connect to the database
-SQLALCHEMY_DATABASE_URI = 'postgresql://brian:admin@localhost:5432/fyyurapp'
-SQLALCHEMY_TRACK_MODIFICATIONS = False
