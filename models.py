f43378ca2eab8262baa32386184ade601499d14a
brian <briansegs@gmail.com>
2021-02-17 13:23:41 -0500

    rm filters

--- models.py
+++ models.py
@@ -29,19 +29,6 @@ def setup_db(app, database_path=database_path):
     app.config.from_object('config')
     db.create_all()
 
-#----------------------------------------------------------------------------#
-# Filters.
-#----------------------------------------------------------------------------#
-
-    def format_datetime(value, format='medium'):
-        date = dateutil.parser.parse(value)
-        if format == 'full':
-            format="EEEE MMMM, d, y 'at' h:mma"
-        elif format == 'medium':
-            format="EE MM, dd, y h:mma"
-        return babel.dates.format_datetime(date, format)
-
-        app.jinja_env.filters['datetime'] = format_datetime
 
 #----------------------------------------------------------------------------#
 # Models.
