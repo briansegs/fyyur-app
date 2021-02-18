de17c55271b3a6f9392b622d561a1ffb4bedb376
brian <briansegs@gmail.com>
2021-02-18 12:38:03 -0500

    rm config

--- backend/config.py
+++
@@ -1,6 +0,0 @@
-import os
-SECRET_KEY = os.urandom(32)
-# Grabs the folder where the script runs.
-# basedir = os.path.abspath(os.path.dirname(__file__))
-
-template_dir = os.path.abspath('../frontend/templates')
